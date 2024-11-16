acab924cd5608869b9aa90098bca9bf221486ebd
areeshaahmed1601 <areeshaahmed1601@gmail.com>
2024-11-17 00:22:08 +0500

    episode 4

--- src/app.js
+++ src/app.js
@@ -2,21 +2,8 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/user", (req, res) => {
-//   res.send("hahahahha");
-// });
-
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Areesha", lastName: "Ahmed" });
-});
-
-app.post("/user", (req, res) => {
-  console.log("Save Data to the database");
-  res.send("Data successfully saved to the database");
-});
-//this will match all the http method api calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from the server..");
+app.get("/ab?c", (req, res) => {
+  res.send("hello");
 });
 
 app.listen("3000", () => {
