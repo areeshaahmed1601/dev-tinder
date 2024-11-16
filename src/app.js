91d991bb12bf43bfeef01f06de378cb2cc0253fa
areeshaahmed1601 <areeshaahmed1601@gmail.com>
2024-11-16 21:55:32 +0500

    Route handling

--- src/app.js
+++ src/app.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+// app.use("/user", (req, res) => {
+//   res.send("hahahahha");
+// });
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Areesha", lastName: "Ahmed" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Save Data to the database");
+  res.send("Data successfully saved to the database");
+});
+//this will match all the http method api calls to /test
 app.use("/test", (req, res) => {
   res.send("Hello from the server..");
 });
