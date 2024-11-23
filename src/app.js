787bf67be1f8a6890e6b060aa8d0fe5ecc30c17f
areeshaahmed1601 <areeshaahmed1601@gmail.com>
2024-11-23 14:50:25 +0500

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -2,9 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.get("/ab?c", (req, res) => {
-  res.send("hello");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Handling the route");
+    next();
+    res.end("Response");
+  },
+  (req, res) => {
+    console.log("Handling the route user 2");
+    res.send("2nd Response!!!");
+  }
+);
+
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("Handling the route");
+//     res.end("Response");
+//     next();
+//   },
+//   (req, res) => {
+//     console.log("Handling the route user 2");
+//     res.send("2nd Response!!!");
+//   }
+// );
 
 app.listen("3000", () => {
   console.log("Server is listening to port 3000");
