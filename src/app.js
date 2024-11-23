6c971eb4ceab3658de0b5d229f18e328e8b3071b
areeshaahmed1601 <areeshaahmed1601@gmail.com>
2024-11-23 19:45:55 +0500

    Error handling

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 const express = require("express");
-const { adminAuth } = require("./middlewares/auth");
 
 const app = express();
 
-//Handle auth middleware for all req
-app.use("/admin", adminAuth);
+app.get("/getUserData", (req, res) => {
+  //logic of DB call and get user data
 
-app.get("/admin/getAllData", (req, res) => {
-  //Logic of checking if the request is authorized
-  res.send("All Data Sent");
+  throw new Error("sfnskfnkfna");
+  res.send("User Data Sent");
 });
 
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("Delete a user");
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Something went wrong");
+  }
 });
 
 app.listen("3000", () => {
